e439852e1381959819085b636fb39bb514a9b490
Gabriel Rosa <gabrielrosa@Gabriels-MacBook-Pro.local>
2020-12-21 23:04:03 -0300

    feat: init

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,22 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <Main />
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Main from './components/Main.vue'
 
 export default {
-  name: "App",
+  name: 'App',
   components: {
-    HelloWorld
+    Main
   }
-};
+}
 </script>
 
 <style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@400;700&display=swap');
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-family: 'Roboto Mono', monospace;
 }
 </style>
---
+++ src/components/Main.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="main">
+    <h3>Kenye West Sentences</h3>
+    <button @click="handleQuote">Generate Quote</button>
+
+    <p v-if="quote">{{ quote }}</p>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'Main',
+  data: () => ({
+    quote: null
+  }),
+  methods: {
+    async handleQuote() {
+      const { data } = await axios.get('https://api.kanye.rest/')
+      this.quote = data.quote
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.main {
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  justify-content: center;
+}
+
+h3 {
+  font-size: 2em;
+}
+
+button {
+  background-color: tomato;
+  border-radius: 5px;
+  border: 1px solid tomato;
+  color: black;
+  cursor: pointer;
+  font-size: 1.5em;
+  margin: 25px auto;
+  max-width: 300px;
+  outline: none;
+  padding: 10px;
+  width: 100%;
+}
+</style>
